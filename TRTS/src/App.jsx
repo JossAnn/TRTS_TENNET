534a247182ab894eddad4782ae404313c3e5e62f
Ranger19 <ranger19x2@gmail.com>
2023-10-09 22:19:31 -0500

    Creacion del proyecto

---
+++ TRTS/src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div className="w-full h-screen grid grid-cols-3 bg-[pink] text-white p-4 text-center">
+      <h1 className="text-2xl col-start-2 text-[green] mt-[50%]">¡Hola, putitas con Tailwind CSS!</h1>
+    </div>
+  );
+}
+
+export default App;
